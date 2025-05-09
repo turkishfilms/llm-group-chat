e13c9fa26467d2cb644e931e54f02b3e21946f82
Reginald Robinson <ddrbnsn@gmail.com>
2025-05-09 14:29:32 -0700

    Cleared unnecessary Styling
    Removed App.css and output.css
    Simplified styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ChatRoom from './ChatRoom.jsx'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -27,7 +14,6 @@ function App() {
         </p>
       </div>
       <p>oh no i atn believe this</p>
-      <ChatRoom></ChatRoom>
     </>
   )
 }
