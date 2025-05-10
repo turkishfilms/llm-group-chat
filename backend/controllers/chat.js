150c035de4f9fc85640237bd40ba8c1474cd78f3
Tabor Holly <89608421+taborgreat@users.noreply.github.com>
2025-05-09 22:50:11 -0700

    idea

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -21,7 +21,7 @@ const getChatMessages = async (req, res) => {
 
 const postNewMessage = async (req, res) => {
   try {
-    const { chatroomId, username, message } = req.body;
+    const { chatroomId, username, message /*, isHuman*/ } = req.body;
 
     if (!chatroomId || !username || !message) {
       return res.status(400).json({ error: "All fields are required" });
@@ -36,6 +36,8 @@ const postNewMessage = async (req, res) => {
     res
       .status(201)
       .json({ message: "Chat message saved", chatMessage: newChatMessage });
+    //this is where the function could be to send chat to llm for response, which once received gets posted
+    // as postNewMessage (needs to know if human or ai posting to prevent infinite ai loop)
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Server error" });
