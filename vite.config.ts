0e223ad214b0aff91481929c6bcaed00f682bf18
Reginald Robinson <ddrbnsn@gmail.com>
2025-05-10 10:58:54 -0700

    Added proxy
    Added proxy from 5173 -> 3000
    connects front and back

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,12 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  server: {
+    proxy: {
+      '/newMessage': 'http://localhost:3000',
+      '/chatMessages': 'http://localhost:3000',
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
