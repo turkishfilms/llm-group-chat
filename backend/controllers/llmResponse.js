b3f38dcdd1300b968f6201550c624ba23c0f4bf8
Tabor Holly <89608421+taborgreat@users.noreply.github.com>
2025-05-15 03:26:15 -0700

    more backend code for editing agent

--- backend/controllers/llmResponse.js
+++ backend/controllers/llmResponse.js
@@ -29,7 +29,7 @@ const getLLMResponse = async (data) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.GROQ_KEY}`,
+          Authorization: `Bearer ${process.env.GROQ_CLOUD_API_KEY}`,
         },
         body: JSON.stringify(payload),
       }
