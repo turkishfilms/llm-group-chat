73b6a1edc536814625bef11c2e0f07253e7860b1
Reginald Robinson <ddrbnsn@gmail.com>
2025-05-16 17:25:33 -0700

    Added working function for username and chatroomid
    Added requisite props
    Makes Landing page functional

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,21 +1,27 @@
-
-//imports
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
+import { Button } from "./components/ui/button"
 
-export default function LandingPage() {
+export default function LandingPage({ setUsername, setChatroomId, handleLandingSend }) {
 
 	return (
 		<>
-			<div >
-				<Card className="max-w-screen max-h-screen h-screen bg-blue-500 text-black-500">
-					<CardContent className="flex items-center justify-center ">
-						<Input placeHolder="Username" className="bg-orange-200 max-w-50"></Input>
-						<Input placeHolder="ChatRoom ID:" className="bg-orange-200 max-w-100"></Input>
-					</CardContent>
-				</Card>
-
-			</div>
+			<Card className="max-w-screen max-h-screen h-screen bg-blue-500 text-black-500">
+				<p>LLMGC</p>
+				<CardContent className="flex items-center justify-center ">
+					<Input
+						placeholder="Username"
+						className="bg-orange-200 max-w-50"
+						onChange={(e) => setUsername(e.target.value)}
+					></Input>
+					<Input
+						placeholder="ChatRoom ID:"
+						className="bg-orange-200 max-w-100"
+						onChange={(e) => setChatroomId(e.target.value)}
+					></Input>
+					<Button onClick={handleLandingSend}></Button>
+				</CardContent>
+			</Card>
 		</>
 
 	)
